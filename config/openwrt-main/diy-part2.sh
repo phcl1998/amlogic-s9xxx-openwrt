445f57245093b8e436c19294f44df1072eb83ae4
phcl1998 <125857645+phcl1998@users.noreply.github.com>
2024-06-06 17:51:21 +0800

    Update diy-part2.sh

--- config/openwrt-main/diy-part2.sh
+++ config/openwrt-main/diy-part2.sh
@@ -33,8 +33,8 @@ git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-a
 # git clone https://github.com/linkease/nas-packages.git;master' package/nas-packages
 # git clone https://github.com/linkease/nas-packages-luci.git package/nas-packages-luci
 
-echo 'src-git nas https://github.com/linkease/nas-packages.git;master' >> feeds.conf.default
-echo 'src-git nas_luci https://github.com/linkease/nas-packages-luci.git;main' >> feeds.conf.default
+# echo 'src-git nas https://github.com/linkease/nas-packages.git;master' >> feeds.conf.default
+# echo 'src-git nas_luci https://github.com/linkease/nas-packages-luci.git;main' >> feeds.conf.default
 
 
 
